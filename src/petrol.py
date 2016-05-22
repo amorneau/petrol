dc74c90711268de1ea3eb9f342de8dec89dee032
Alex Morneau <amorneau@protonmail.com>
2016-05-22 00:56:26 -0700

    Add runner script

---
+++ src/petrol.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+from post import Post
+from writer import Writer
+
+import sys
+
+def print_usage():
+    usage_message = 'petrol <input_file1> [<input_file2> ...]'
+    print(usage_message)
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        print_usage()
+        sys.exit(1)
+
+    writer = Writer()
+
+    for file_name in sys.argv[1:]:
+        with open(file_name, 'r') as input_file:
+            file_contents = input_file.read()
+
+            post = Post(file_contents)
+            writer.write_post(post)
